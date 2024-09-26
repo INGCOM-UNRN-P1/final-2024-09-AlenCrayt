d2345f62c7247d8abcf96e35365a5bb63fa3392a
AlenCrayt <agus.santisteban@gmail.com>
2024-09-26 16:46:59 -0300

    Agregado filtrado de duplicados y creacion de archivo

--- lector_archivos.c
+++ lector_archivos.c
@@ -1,36 +1,20 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
+#include <time.h>
 
 char *nombre_archivo;
 size_t memoria_asignada = 2;
 int i = 0;
 
-struct Nodo
-{
-    char *palabra;
-    struct Nodo* siguiente;
-};
+void eliminar_string(char **strings, int *cantidad_de_strings, int indice) {
+    free(strings[indice]);
 
-struct Nodo* nodo_nuevo(char palabra_recibida[25], struct Nodo* proximo_nodo)
-{
-    struct Nodo* nodo = malloc(sizeof(struct Nodo));
-    nodo->palabra = palabra_recibida;
-    nodo->siguiente = proximo_nodo;
-    return nodo;
-}
-
-struct Node* constructList(char palabras[], int n)
-{
-    struct Node *cabeza = NULL, *nodo = NULL;
-
-    for (int i = n - 1; i >= 0; i--)
-    {
-        nodo = nodo_nuevo(palabras, nodo);
-        cabeza = nodo;
+    for (int i = indice; i < *cantidad_de_strings - 1; i++) {
+        strings[i] = strings[i + 1];
     }
- 
-    return cabeza;
+
+    (*cantidad_de_strings)--;
 }
 
 int main()
@@ -47,42 +31,68 @@ int main()
         i += 1;
     }
     nombre_archivo[i] = '\0';
-    printf("Asignado %lu bytes de memoria para el nombre del archivo\n", memoria_asignada);
     printf("Buscando: %s\n", nombre_archivo);
-    char word[25];
+    char word[25] = {NULL};
+    char **lista_palabras = NULL;
     int p_1;
     int z = 0;
+    int cantidad_de_palabras = 0;
     //A base de un espacio hay que separar las palabras individualmente
     FILE *archivo_abierto = fopen(nombre_archivo, "r");
     fseek(archivo_abierto, 0, SEEK_END);
-    //printf("Longitud de archivo: %lu\n", (ftell(archivo_abierto) * sizeof(char)));
     long largo_archivo = ftell(archivo_abierto);
     char *buffer = malloc((largo_archivo * sizeof(char)));
     fseek(archivo_abierto, 0, SEEK_SET);
-    size_t cantidad_palabra = fread(buffer, sizeof(char), largo_archivo, archivo_abierto);
-    printf("Longitud en caracteres del archivo: %lu\n", cantidad_palabra);
-    for (size_t n = 0; strlen(buffer) >= n; n++) {
-        //la violacion de segmento que esta ocurriendo parece ser por que el contador esta yendo mas alla de los limites de longitud del archivo
-        //el iterador se ejecuta pero no puede leer el contenido de los valores del buffer con lo cual no se llevan a cabo las operaciones
-        //tendría que usar p_! como el espacio al principio y p_2 como el espacio al final de una palabra para asi extraer una palabra individual del archivo
-        //printf("%lu\n", n);
-        if (buffer[n] == ' ') {
-            p_1 = n + 1;//como conseguir extraer la primera palabra si no hay un espacio al principio?
-            //printf("%d\n", p_1);
-            while (buffer[p_1] != ' ' && buffer[p_1] != EOF && buffer[p_1] != '\n') {//extrae la segunda palabra correctamente pero produce errores con el resto
+    size_t cantidad_de_caracteres = fread(buffer, sizeof(char), largo_archivo, archivo_abierto);
+    for (size_t n = 0; strlen(buffer) >= n; n++) 
+    {
+        if (buffer[n] == ' ') 
+        {
+            p_1 = n + 1;
+            while (buffer[p_1] != ' ' && buffer[p_1] != EOF && buffer[p_1] != '\n') 
+            {
                 word[z] = buffer[p_1];
-                z += 1;//el problema parece ser que como encuentra el primer espacio se ejecuta el iterador while incluso si es el ultimo espacio disponible en el archivo
+                z += 1;
                 p_1 += 1;
-                //ya que z es redeclarado adentro del if entonces se sobreescriben las palabra ya existes en el arreglo word
-            }//con la palabras separadas obtenidas, hay que guardarlas en una lista enlazada y filtrar por duplicados
-            printf("%s\n", word);
-            memset(word, '\0', sizeof(word));
+            }
+            cantidad_de_palabras += 1;
+            lista_palabras = realloc(lista_palabras , cantidad_de_palabras * sizeof(char*));
+            lista_palabras[cantidad_de_palabras - 1] = malloc(strlen(word) + 1);
+            strcpy(lista_palabras[cantidad_de_palabras - 1], word);
+            memset(word, '\0', strlen(word));
             z = 0;
-            //printf("Escaneado\n");
-        }//como seguir con el recorrido por el arreglo hasta llegar al proximo espacio?
+        }
 
     }
-    printf("programa ejecutado\n");
+
+    free(buffer);
+
+    for (int i = 0; i < cantidad_de_palabras;) 
+    {
+        int x = i + 1;
+        while (x < cantidad_de_palabras)
+        {
+            if (strcmp(lista_palabras[i], lista_palabras[x]) == 0) 
+            {
+                eliminar_string(lista_palabras, &cantidad_de_palabras, x);
+                cantidad_de_caracteres -= 1;
+            }
+            x++;
+        }
+        i++;
+    }
+    FILE *nuevo_archivo = fopen("archivo_filtrado.txt", "w");
+    for (int i = 0; i < cantidad_de_palabras; i++) 
+    {
+        fprintf(nuevo_archivo, "%s\n", lista_palabras[i]);
+    }
+    for (int i = 0; i < cantidad_de_palabras; i++) 
+    {
+        free(lista_palabras[i]);
+    }
+    fclose(nuevo_archivo);
+    printf("El archivo %s ha sido filtrado de duplicados\n", nombre_archivo);
+    free(lista_palabras);
     free(nombre_archivo);
     return 0;
 }
